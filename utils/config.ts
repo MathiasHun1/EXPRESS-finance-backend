0176d2dbbba9e91cd9d499c09f2789d08108e568
snus4me <mathias.hun@gmail.com>
2025-09-22 23:41:56 +0200

    fix bug: production env variable

--- utils/config.ts
+++ utils/config.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-const MONGODB_URI = process.env.NODE_ENV === 'test' ? process.env.MONGODB_URI_TEST : process.env.MONGODB_URI;
+const MONGODB_URI = process.env.NODE_ENV === 'test' ? process.env.MONGODB_URI_TEST : process.env.MONGODB_URI_PRODUCTION;
 
 export default {
   PORT,
